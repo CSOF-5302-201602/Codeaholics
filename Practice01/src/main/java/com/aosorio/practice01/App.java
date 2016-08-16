ed42ce200bb6dcebcf782bd4edbdca0505f6e58b
andres0sorio <osorio.af@gmail.com>
2016-08-15 21:33:22 -0500

    [test] testing Netbeans and GIT

---
+++ Practice01/src/main/java/com/aosorio/practice01/App.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.aosorio.practice01;
+
+/**
+ *
+ * @author AOSORIO
+ */
+public class App {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        System.out.println("Hello world!");
+        
+        
+        
+        
+    }
+    
+}
